9ed300fbef80d209ea4c96d6f613243103956d19
Michal Kahle <michalkahle@gmail.com>
2020-09-24 13:15:27 +0200

    Streamlining

--- rtca.py
+++ rtca.py
@@ -14,7 +14,6 @@ from functools import partial
 import scipy.cluster.hierarchy
 from sklearn import metrics
 scipy.cluster.hierarchy.set_link_color_palette(['gray', 'goldenrod'])
-# scipy.cluster.hierarchy.set_link_color_palette(['salmon', 'skyblue'])
 # from IPython.core.debugger import set_trace
 from plotnine import *
 from time import time
@@ -170,7 +169,60 @@ def load_file(filename):
     org = org[['tp', 'welln', 'dt', 'org']]
     return org
 
+tts = np.cumsum(np.concatenate([[0], 1.05**np.arange(43) * .5]))
+
+def resample_plate(plate, tts=tts, column='lnci'):
+    well = plate.query('welln == 0')
+    source_t = well['time'].values
+    ii = np.searchsorted(source_t, tts)
+    t1, t2 = source_t[ii-1], source_t[ii]
+
+    index = [c for c in
+        ['cl', 'cp', 'ap', 'welln', 'library', 'compound', 'sid', 'log_c', 'tp']
+        if c in plate.columns]
+    plate2 = plate.set_index(index)[column].unstack()
+    tps = well['tp'].iloc[ii]
+    c1 = plate2[tps - 1].values
+    c2 = plate2[tps].values
+    res = c1 + (c2 - c1) * ((tts - t1)/(t2 - t1))
+    columns = pd.MultiIndex.from_product([[column], tts], names=[None, 'time'])
+    return pd.DataFrame(res, index=plate2.index, columns=columns).stack().reset_index()
+
 _rows = np.array([chr(x) for x in range(65, 91)] + ['A' + chr(x) for x in range(65, 71)])
+
+def welln2well_384(wells, form=384):
+    form = int(form)
+    if form not in [96, 384, 1536]:
+        raise ValueError('Only formats 96, 384 and 1536 supported.')
+    n_cols = int(sqrt(form/2*3))
+
+    wells = wells if type(wells) == np.ndarray else np.array(wells, dtype=np.int)
+    if np.any(wells >= form) or np.any(wells < 0):
+        raise ValueError('welln out of range')
+    rr = _rows[wells // n_cols]
+    cc = (wells % n_cols + 1).astype(str)
+    return np.core.defchararray.add(rr, cc)
+
+def well2welln_384(wells, form=384):
+    form = int(form)
+    if form not in [96, 384, 1536]:
+        raise ValueError('Only formats 96, 384 and 1536 supported.')
+    n_cols = int(sqrt(form/2*3))
+    wells = wells if type(wells) == np.ndarray else np.array(wells, dtype=np.str)
+    _well_regex = re.compile('^([A-Z]{1,2})(\d{1,2})')
+    def _w2wn(well, n_cols):
+        match = _well_regex.match(well)
+        if not match:
+            raise ValueError('Well not recognized: "%s"' % well)
+        rr, cc = match.group(1), match.group(2)
+        rrn = ord(rr) - 65 if len(rr) == 1 else ord(rr[1]) - 39
+        ccn = int(cc) - 1
+        return rrn * n_cols + ccn
+    _vw2wn = np.vectorize(_w2wn, excluded=('n_cols'))
+    wns = _vw2wn(wells, n_cols)
+    if np.any(wns >= form) or np.any(wns < 0):
+        raise ValueError('welln out of range')
+    return wns
 
 # Plotting ####################################################
 
@@ -223,7 +275,6 @@ def plot(df, x='time', y='nci', color=None):
     else:
         groups = df[color].unique()
         cmap = plt.get_cmap('gist_rainbow')
-        # cmap = plt.get_cmap('viridis')
         color_map = dict(zip(groups, cmap((groups - groups.min()) / (groups.max()-groups.min()))))
 
         for (cc, well), group in df.groupby([color, 'welln']):
@@ -247,9 +298,7 @@ def plot3d(dd, color=None, factor=None, cmap=None, hover=None, projection='UMAP'
     layout = {'height': 600, 'margin': {'b': 0, 'l': 0, 'r': 0, 't': 0}, 
               'paper_bgcolor': '#f0f0f0', 'width': 800,
               'scene': {'xaxis':{'title':xc}, 'yaxis':{'title':yc}, 'zaxis':{'title':zc}}}
-
     traces = []
-
 
     if factor == False or dd[color].dtype == 'float64':
         if dd[color].dtype.name == 'category':
@@ -283,133 +332,76 @@ def plot3d(dd, color=None, factor=None, cmap=None, hover=None, projection='UMAP'
             layout['showlegend'] = True
 
     fig = go.Figure(data=traces, layout=go.Layout(layout))
-#     if publish:
-#         plotly.iplot(fig)
-#     else:
     plotly.offline.iplot(fig)
 
+# PCA ###########################################################################
 
-def pca(dfl, n=3, plot=True, x='lnci', t='tp'):
-    dfw = prepare_unstack(dfl, x=x, t=t).unstack(t)
-    pca_m = PCA(n_components=n)
-    X_pca = pca_m.fit_transform(dfw.values)
-    columns = ['PC' + str(x) for x in range(1,n+1)]
-    X_pca_df = pd.DataFrame(X_pca, index=dfw.index, columns=columns).reset_index()
+# def pca(df, n=3, plot=True):
+#     dfi = set_index(df)
+#     model = PCA(n_components=n, svd_solver='full')
+#     PCs = model.fit_transform(dfi.values)
+#     PCs_df = pd.DataFrame(PCs, index=dfi.index, columns=range(n)).reset_index()
+#     if plot:
+#         plot_explained_variance(model)
+#     return model, PCs_df
 
-    if plot:
-        plot_explained_variance(pca_m)
-    # print('residual = %.3f' % (1-pca_m.explained_variance_ratio_.sum()))
-    return pca_m, X_pca_df
+# def pca_filter(df, n=3, plot=True):
+#     model, X_pca_df = pca(df, n, plot)
+    
+#     X_components_df = pd.DataFrame(model.components_.T,
+#         index=dfw.columns.droplevel(),
+#         columns=pd.MultiIndex.from_product([['w'], range(1,n+1)], names=[None, 'pc'])
+#     )
 
-def pca_wide(dfw, n=3, plot=True):
-    X = extract_data(dfw)
-    pca_m = PCA(n_components=n)
-    X_pca = pca_m.fit_transform(X)
-    columns = ['PC' + str(x) for x in range(1,n+1)]
-    X_pca_df = pd.DataFrame(X_pca, index=dfw.index, columns=range(n))#.reset_index()
+#     F = model.inverse_transform(X_pca)
 
-    if plot:
-        plot_explained_variance(pca_m)
-    # print('residual = %.3f' % (1-pca_m.explained_variance_ratio_.sum()))
-    return pca_m, X_pca_df#.reset_index()
+#     filtered = pd.DataFrame(F, index=dfw.index, columns=dfw.columns)
+#     residual = dfw - filtered
+#     if plot:
+#         rtca.plot_explained_variance(model)
+#     return X_components_df, filtered, residual
 
-def pca_fit(dfl, n=3, plot=True, x='lnci', t='tp'):
-    dfw = prepare_unstack(dfl, x=x, t=t).unstack(t)
-    pca_m = PCA(n_components=n)
-    X_pca = pca_m.fit_transform(dfw.values)
 
-    # columns = ['PC' + str(x) for x in range(1,n+1)]
-    # X_pca_df = pd.DataFrame(X_pca, index=dfw.index, columns=columns).reset_index()
 
-    if plot:
-        plot_explained_variance(pca_m)
-    # print('residual = %.3f' % (1-pca_m.explained_variance_ratio_.sum()))
-    return pca_m
+# def set_index(df):
+#     cols = [col for col in df.columns if type(col) != int]
+#     return df.set_index(cols)
 
-def pca_components(model, time_index=None):
-    shape = model.components_.shape
-    if time_index is None:
-        time_index = pd.Index(range(1, shape[1]+1), name='tp')
-    else:
-        time_index = pd.Index(time_index, name='time')
-    return pd.DataFrame(model.components_.T,
-        index=time_index,
-        columns=pd.MultiIndex.from_product([['w'], range(1,shape[0]+1)], names=[None, 'PC'])
-        ).stack().reset_index()
+# def reset_index(df):
+#     return df.reset_index()
 
-def pca_reconstruct(model, df_pca, time_index=None):
-    shape = model.components_.shape
-    if time_index is None:
-        time_index = range(1, shape[1]+1)
-    time_index = pd.MultiIndex.from_product([['lnci'], time_index], names=[None, 'time'])
-    df_pca.set_index(list(df_pca.columns[~df_pca.columns.str.contains('PC')]), inplace=True)
-    filtered = model.inverse_transform(df_pca.values)
-    df = pd.DataFrame(filtered, index=df_pca.index, columns=time_index)
-    return df.stack().reset_index()
 
-def pca_filter(dfl, n=3, plot=True, x='lnci', t='tp'):
-    dfl = dfl.sort_values(['welln', t]).reset_index(drop=True)
-    dfl = prepare_unstack(dfl, x=x, t=t)
-    dfw = dfl.unstack(t)
-    pca_m = PCA(n_components=n)
-    X_pca = pca_m.fit_transform(dfw.values)
-    X_components_df = pd.DataFrame(pca_m.components_.T,
-        index=dfw.columns.droplevel(),
-        columns=pd.MultiIndex.from_product([['w'], range(1,n+1)], names=[None, 'pc'])
-        ).stack().reset_index()
 
-    filtered = pca_m.inverse_transform(X_pca)
-    dfl['filtered'] = pd.DataFrame(filtered, index=dfw.index, columns=dfw.columns).stack(t)
-    dfl['residual'] = dfl[x] - dfl['filtered']
 
-    if plot:
-        plot_explained_variance(pca_m)
-    # print('residual = %.3f' % (1-pca_m.explained_variance_ratio_.sum()))
-    return dfl.reset_index(), X_components_df
+# def prepare_unstack(dfw, x='lnci', t='tp'):
+#     to_drop = {'time', 'ci', 'nci', 'lci', 'lnci'} & set(dfw.columns)
+#     if x in to_drop:
+#         to_drop.remove(x)
+#     if t == 'time':
+#         to_drop.remove(t)
+#     dfw = dfw.drop(to_drop, axis=1)
+#     dfw.set_index(
+#         [cc for cc in dfw.columns if (cc != x) & (cc != t)] + [t], inplace=True)
+#     return dfw
 
-def prepare_unstack(dfw, x='lnci', t='tp'):
-    to_drop = {'time', 'ci', 'nci', 'lci', 'lnci'} & set(dfw.columns)
-    if x in to_drop:
-        to_drop.remove(x)
-    if t == 'time':
-        to_drop.remove(t)
-    dfw = dfw.drop(to_drop, axis=1)
-    dfw.set_index(
-        [cc for cc in dfw.columns if (cc != x) & (cc != t)] + [t], inplace=True)
-    return dfw
-
-def plot_explained_variance(pca_m):
-    evr = pca_m.explained_variance_ratio_
-    residual = 1-evr.sum()
-    n = pca_m.n_components
-    ind = np.arange(n+1, 0, -1)
-    col = np.r_[np.tile('g', n), np.array(['r'])]
-    evr = np.r_[evr, np.array([residual])]
-    fig, ax = plt.subplots(figsize=(4,1))
-    ax.barh(ind, evr, color=col)
-    ax.set_yticks(ind)
-    ax.set_yticklabels(['PC' + str(x) for x in range(1, n + 1)] + ['residual'])
-    ax.set_xlim(0,1)
-    ax.set_xlabel('explained variance')
-    ax.set_ylabel('components')
-
+    
 def extract_data(df):
     ints = [col for col in df.columns if type(col) == int]
     if len(ints) > 2:
         selection = df[ints]
     elif df.columns.str.contains('PC').sum() > 3:
         selection = df.loc[:, df.columns.str.contains('PC')]
-    elif df.columns.str.contains('TDRC').sum() > 3:
-        selection = df.loc[:, df.columns.str.contains('TDRC')]
+    elif df.columns.str.contains('UMAP').sum() > 1:
+        selection = df.loc[:, df.columns.str.contains('UMAP')]
     else:
-        raise ValueError('Neither integers, PC or TDRC found in columns.')
+        raise ValueError('Neither integers, PC or UMAP found in columns.')
     X = selection.values
     return X
 
-def add_tsne(df, dims=2, perplexity=30):
+def add_tsne(df, dims=2, perplexity=30, seed=None):
     from sklearn.manifold import TSNE
     X = extract_data(df)
-    X_embedded = TSNE(n_components=dims, perplexity=perplexity).fit_transform(X)
+    X_embedded = TSNE(n_components=dims, perplexity=perplexity, random_state=seed).fit_transform(X)
     for n in range(dims):
         label = 'tSNE%s' % (n+1)
         df[label] = X_embedded[:, n]
@@ -424,70 +416,19 @@ def add_umap(df, dims=3, **kwargs):
     return df
 
 
-def calculate_z_score(p, components=None):
-    res = np.zeros_like(p.PC1)
-    for pc in p.columns[p.columns.str.contains('PC')][:components]:
-        res += ((p[pc] - p[pc].mean())/ p[pc].std())**2
-    return np.sqrt(res)
+# def calculate_z_score(p, components=None):
+#     res = np.zeros_like(p.PC1)
+#     for pc in p.columns[p.columns.str.contains('PC')][:components]:
+#         res += ((p[pc] - p[pc].mean())/ p[pc].std())**2
+#     return np.sqrt(res)
 
-def add_znorm(df):
-    znorm = np.zeros(df.shape[0])
-    for pc in [col for col in df.columns if 'PC' in col]:
-        znorm += (df[pc] / df[pc].std())**2
-    df['znorm'] = np.sqrt(znorm)
+# def add_znorm(df):
+#     znorm = np.zeros(df.shape[0])
+#     for pc in [col for col in df.columns if 'PC' in col]:
+#         znorm += (df[pc] / df[pc].std())**2
+#     df['znorm'] = np.sqrt(znorm)
 
-tts = np.cumsum(np.concatenate([[0], 1.05**np.arange(43) * .5]))
-
-def resample_plate(plate, tts=tts, column='lnci'):
-    well = plate.query('welln == 0')
-    source_t = well['time'].values
-    ii = np.searchsorted(source_t, tts)
-    t1, t2 = source_t[ii-1], source_t[ii]
-
-    index = [c for c in
-        ['cl', 'cp', 'ap', 'welln', 'library', 'compound', 'sid', 'log_c', 'tp']
-        if c in plate.columns]
-    plate2 = plate.set_index(index)[column].unstack()
-    tps = well['tp'].iloc[ii]
-    c1 = plate2[tps - 1].values
-    c2 = plate2[tps].values
-    res = c1 + (c2 - c1) * ((tts - t1)/(t2 - t1))
-    columns = pd.MultiIndex.from_product([[column], tts], names=[None, 'time'])
-    return pd.DataFrame(res, index=plate2.index, columns=columns).stack().reset_index()
-
-def welln2well_384(wells, form=384):
-    form = int(form)
-    if form not in [96, 384, 1536]:
-        raise ValueError('Only formats 96, 384 and 1536 supported.')
-    n_cols = int(sqrt(form/2*3))
-
-    wells = wells if type(wells) == np.ndarray else np.array(wells, dtype=np.int)
-    if np.any(wells >= form) or np.any(wells < 0):
-        raise ValueError('welln out of range')
-    rr = _rows[wells // n_cols]
-    cc = (wells % n_cols + 1).astype(str)
-    return np.core.defchararray.add(rr, cc)
-
-def well2welln_384(wells, form=384):
-    form = int(form)
-    if form not in [96, 384, 1536]:
-        raise ValueError('Only formats 96, 384 and 1536 supported.')
-    n_cols = int(sqrt(form/2*3))
-    wells = wells if type(wells) == np.ndarray else np.array(wells, dtype=np.str)
-    _well_regex = re.compile('^([A-Z]{1,2})(\d{1,2})')
-    def _w2wn(well, n_cols):
-        match = _well_regex.match(well)
-        if not match:
-            raise ValueError('Well not recognized: "%s"' % well)
-        rr, cc = match.group(1), match.group(2)
-        rrn = ord(rr) - 65 if len(rr) == 1 else ord(rr[1]) - 39
-        ccn = int(cc) - 1
-        return rrn * n_cols + ccn
-    _vw2wn = np.vectorize(_w2wn, excluded=('n_cols'))
-    wns = _vw2wn(wells, n_cols)
-    if np.any(wns >= form) or np.any(wns < 0):
-        raise ValueError('welln out of range')
-    return wns
+# Clustering ######################################################################################
 
 def hierarchical_clustering(df, k=10, plot=False, figsize=(8, 8)):
     X = extract_data(df)
@@ -531,16 +472,14 @@ def hierarchical_clustering(df, k=10, plot=False, figsize=(8, 8)):
 
     df['cluster'] = pd.Categorical(clusters)
     #return fig, ax
-
-def evaluate_clustering(df, dr_method, k_min=10, k_max=30):
-    fn = 'clustering_comparisons.csv'
     
+fn = 'clustering_comparisons.csv'
+    
+def evaluate_clustering(df, dr_method, k_min=10, k_max=30):
     if os.path.isfile(fn):
         ec = pd.read_csv(fn)
     else:
         ec = pd.DataFrame()
-        
-        
     X = extract_data(df)
     Z = scipy.cluster.hierarchy.linkage(X, 'ward')
     loc = df['moa'] != 'other'
@@ -555,10 +494,14 @@ def evaluate_clustering(df, dr_method, k_min=10, k_max=30):
           .drop_duplicates(['dr', 'k', 'index'],keep='last')
           .sort_values(['dr', 'k', 'index']))
     ec.to_csv(fn, index=False) 
-    return ec
 
+def remove_evaluation(method):
+    ec = pd.read_csv(fn)
+    ec = ec.query('dr != @method')
+    ec.to_csv(fn, index=False)
+    
 def plot_comparisons():
-    ec = pd.read_csv('clustering_comparisons.csv')
+    ec = pd.read_csv(fn)
     (ggplot(ec)
      + aes('k', 'value', color='index')
      + geom_line()
@@ -566,7 +509,19 @@ def plot_comparisons():
      + theme(figure_size=(8, 2))
     ).draw()
     
+def plot_comparisons2():
+    ec = pd.read_csv(fn)
+    (ggplot(ec)
+     + aes('k', 'value', color='dr')
+     + geom_line()
+     + facet_grid('~index')
+     + theme(figure_size=(8, 2))
+    ).draw()
+    
+    
+
 # TDRC ####################################################
+
 inflectionShift = 6
 slopeFactorHill = 10
 
